385b0063f935e8eea0544cdd1888432483ee85d8
TudorDan <tudor.ist@gmail.com>
2022-05-12 10:46:36 +0300

    Small fix for program entry point

--- src/main/java/com/codecool/fiveinarow/FiveInARow.java
+++ src/main/java/com/codecool/fiveinarow/FiveInARow.java
@@ -3,10 +3,11 @@ package com.codecool.fiveinarow;
 public class FiveInARow {
 
     public static void main(String[] args) {
+
         Game game = new Game(11, 11);
 //        game.enableAi(1);
 //        game.enableAi(2);
-        game.printBoard();
         game.play(5);
+
     }
 }
